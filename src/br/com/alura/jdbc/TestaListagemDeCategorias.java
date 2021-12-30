955796f4ea55004c75b01b9ca5dcfe8e5b64ee49
Diego Rossi Corrêa <diegorossicorrea2000@gmail.com>
2021-12-30 00:35:44 -0300

    Adicionando entidade categoria

---
+++ src/br/com/alura/jdbc/TestaListagemDeCategorias.java
@@ -0,0 +1,25 @@
+package br.com.alura.jdbc;
+
+import br.com.alura.jdbc.dao.CategoriaDAO;
+import br.com.alura.jdbc.factory.ConnectionFactory;
+import br.com.alura.jdbc.modelo.Categoria;
+import br.com.alura.jdbc.modelo.Produto;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TestaListagemDeCategorias {
+    public static void main(String[] args) throws SQLException {
+        try(Connection connection = new ConnectionFactory().recuperarConexão()) {
+            CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
+            List<Categoria> listaDeCategoria = categoriaDAO.listarComProdutos();
+            listaDeCategoria.forEach(ct -> {
+                System.out.println(ct.getNome());
+                for(Produto produto : ct.getProdutos()) {
+                    System.out.println(ct.getNome() + " - " + produto.getNome());
+                }
+            });
+        }
+    }
+}
